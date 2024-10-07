23a5d5b22697de8b914b871f50f097eac6e13533
SeUkKim <eleccookie@gmail.com>
2024-10-07 19:57:44 +0900

    add get-current-time

---
+++ get-current-time/README.md
@@ -0,0 +1,27 @@
+# get-current-time
+
+A github action that sets the curent ISO8601 time with formated value. It is used to obtain build time or the time of a specific event.
+
+## Example usage
+```yaml
+    steps:
+      - name: Get current time
+        uses: initia-labs/actions/get-current-time@main
+        id: current-time
+        with:
+            format: YYYY-MM-DDTHH-mm-ss
+            
+      - name: Copy changed files to ...
+        env:
+          CURRENT_TIME: ${{ steps.current-time.outputs.formattedTime }}
+```
+
+## Variables
+* format: [optional] time format to use
+* timezone: [optional] timezone to use
+* ustcOffset: [optional] UTC offset to use
+
+## Outputs
+* time: ISO time
+* formattedTime: formatted time using format and utcOffset varialbes
+* year, month, day, hour, minute, second, millisecond: the value representing as its name
---
+++ get-current-time/index.js
@@ -0,0 +1,32 @@
+const core = require('@actions/core')
+const moment = require('moment-timezone')
+
+function getCurrentTime() {
+  try {
+    const utcOffset = core.getInput('utcOffset', {required: false})
+    const format = core.getInput('format', {required: false})
+    const timezone = core.getInput('timezone', {required: false})
+
+    let time = moment()
+    if (timezone) time = time.tz(timezone)
+    else if (utcOffset) time = time.utcOffset(utcOffset)
+    else time = time.utcOffset('+00:00')
+
+    core.setOutput('time', time.toISOString())
+    core.setOutput('formattedTime', time.format(format))
+
+    let [year, month, day, hour, minute, second, millisecond] = time.toArray()
+    month = Number(month) + 1
+    core.setOutput('year', year)
+    core.setOutput('month', month)
+    core.setOutput('day', day)
+    core.setOutput('hour', hour)
+    core.setOutput('minute', minute)
+    core.setOutput('second', second)
+    core.setOutput('millisecond', millisecond)
+  } catch (e) {
+    core.setFailed(e.message)
+  }
+}
+
+getCurrentTime()
